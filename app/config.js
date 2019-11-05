84b2d85e51d087734860fe3b864e3df1ae1d6adc
Kev Keenoy <kevin.keenoy@cabinetoffice.gov.uk>
2019-11-05 11:08:48 +0000

    Update service title

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'MHCLG EPB',
+  serviceName: 'Energy performance of buildings',
 
   // Default port that prototype runs on
   port: '80',
